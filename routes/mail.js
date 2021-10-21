84f544a036b32b0c04d190f022467aa7ed05cd89
lingul <57398223+lingul@users.noreply.github.com>
2021-10-21 21:02:03 +0200

    Update mail.js

--- routes/mail.js
+++ routes/mail.js
@@ -1,7 +1,7 @@
 const formData = require('form-data');
 const Mailgun = require('mailgun.js');
-const API_KEY = '2b5d63ef15cac74bb5016ee9312b8811-2bf328a5-fb0d8a70';
-const DOMAIN = 'https://api.mailgun.net/v3/sandboxad00b18c49cc4918873a27723aa294e2.mailgun.org/messages';
+const API_KEY = 'XXXXXXXXXX';
+const DOMAIN = 'XXXXXXXXXXX';
 
 var express = require('express');
 var router = express.Router();
@@ -39,15 +39,3 @@ router.post('/', (req, res, next) => {
 });
   
 module.exports = router;
-  
-  /*
-  curl -s --user 'api:2b5d63ef15cac74bb5016ee9312b8811-2bf328a5-fb0d8a70' \
-  https://api.mailgun.net/v3/sandboxad00b18c49cc4918873a27723aa294e2.mailgun.org/messages \
-      -F from='Linnea <mailgun@sandboxad00b18c49cc4918873a27723aa294e2.mailgun.org>' \
-      -F to=linneagullmak96@gmail.com \
-      -F subject='Inbjudan till redigering' \
-      -F text='Registrera dig här. Kopera länken (https://jsramverk-editor-ligm19.azurewebsites.net/register) och klista in i valfri webbläsare.'
-  {
-    "message": "Sandbox subdomains are for test purposes only. Please add your own domain or add the address to authorized recipients in Account Settings."
-  }%
-  */
